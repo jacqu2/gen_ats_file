2466913cb64ade34e52306debd8a815acd9c91d0
Jackie Smedley <jacqu2@umbc.edu>
2021-11-23 08:09:19 -0500

    added timestamp generator function

--- gen_ats_file_delay.rb
+++ gen_ats_file_delay.rb
@@ -7,14 +7,6 @@ require 'date'
 require 'io/console'
 require 'tzinfo'
 
-######################### converts time input string to unix timestamp integer ##################
-# parameter is string execution date/time (MM/DD/YYYY HH:MM:SS)
-def time_str_to_unix(input_time)
-  calc_time = Time.new(input_time[6, 4].to_i, input_time[0, 2].to_i, input_time[3, 2].to_i, input_time[11, 2].to_i, input_time[14, 2].to_i, input_time[17, 2].to_i).to_i
-  return calc_time
-end
-#################################################################################################
-
 ############################## calculates start time given current time and offset###############
 def calc_start_time(time_delay)
   hours = time_delay[0, 2].to_i
@@ -27,18 +19,17 @@ end
 #################################################################################################
 
 #################### converts input time to epoch time in 32-bit seconds#########################
-# parameter is string execution date/time (MM/DD/YYYY HH:MM:SS)
+# parameter is start time object
 def conv_epoch(input_time)
   # (input time in seconds since 1970 - 1980 unix timestamp), gives input time in seconds since 1980 epoch
   calc_time = input_time.to_i - 315532800
-  puts "Unix timestamp decimal: #{input_time.to_i}"
+
   #use following line instead of other calc_time for unix epoch (1970)
   #calc_time = input_time.to_i
 
-  puts "converted time in decimal: #{calc_time}"
   time_hex = calc_time.to_s(16)
   debug = Time.at(calc_time)
-  puts "Readable Time (should be 10 years off and in utc): #{debug}, time_hex: #{time_hex}"
+  puts "Readable Time (unix): #{debug}, time_hex: #{time_hex}"
   return time_hex
 end
 #################################################################################################
@@ -72,16 +63,12 @@ end
 ############# generates an array of n timestamps a given amount of seconds apart ################
 # inputs are number of timestamps to be generated, interval between each timestamp, and the start
 # timestamp (output of conv_epoch method)
-def gen_timestamps(num_timestamps, seconds_apart, start_timestamp)
-  i = 1
+def gen_timestamps(num_cmds, seconds_apart, start_time_object)
+  i = 0
   times_array = []
-  puts "starting timestamp is #{start_timestamp}"
-  # generate first timestamp
-  times_array[0] = (start_timestamp.to_i + seconds_apart).to_s(16)
-
-  # generate remaining timestamps
-  while i < num_timestamps
-    times_array[i] = (times_array[i - 1].to_i + seconds_apart).to_s(16)
+  # generate timestamps
+  while i < num_cmds
+    times_array[i] = conv_epoch(start_time_object + seconds_apart*(i + 1))
     i = i + 1
   end
   puts times_array
@@ -118,28 +105,30 @@ while time_invalid == 1
   end
 end
 
-
 file_out = file_in + "-r1"
 
 # epoch and hex time conversion
 time_converted = conv_epoch(input_time)
-timestamps_array = gen_timestamps(4, 5, time_converted)
-
 
 #save hex contents of file to string
 str_data = hex_file_to_str(file_in)
 
-#find time locations
+#find time locations and number of commands
 i = 0
+num_cmds = 0
 time_indx = []
 str_data.each_char do |char|
   if char == "1" && str_data[i + 1, 3] == "898"
     if i - 8 >= 0
-      time_indx << i - 8      
+      time_indx << i - 8   
+      num_cmds = num_cmds + 1   
     end
   end
   i = i + 1
 end
+
+# generate timestamps
+ts_array = gen_timestamps(num_cmds, 5, input_time)
 
 # replace times in string
 time_indx.each do |index|
