3b491278e141cdcf9f4514c51200a58c9b660f82
Jackie Smedley <jacqu2@umbc.edu>
2021-11-29 16:23:59 -0500

    added tlm time offset

--- gen_ats_file_delay.rb
+++ gen_ats_file_delay.rb
@@ -27,7 +27,7 @@ def conv_epoch(input_time)
   epoch_day = 01
   epoch_month = 01
   epoch_offset = Time.new(epoch_year, epoch_month, epoch_day).to_i
-  calc_time = input_time.to_i - epoch_offset
+  calc_time = input_time.to_i - epoch_offset + 17340 #time diff between telemetry and tool
 
   #use following line instead of other calc_time for unix epoch (1970)
   #calc_time = input_time.to_i
