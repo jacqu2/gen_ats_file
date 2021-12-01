2a593af142544542ba3663249547ccbd0152aeb1
Jackie Smedley <jacqu2@umbc.edu>
2021-12-01 08:44:07 -0500

    re-added gmt offset

--- gen_ats_file_delay.rb
+++ gen_ats_file_delay.rb
@@ -27,7 +27,8 @@ def conv_epoch(input_time)
   epoch_day = 01
   epoch_month = 01
   epoch_offset = Time.new(epoch_year, epoch_month, epoch_day).to_i
-  calc_time = input_time.to_i - epoch_offset 
+  gmt_offset = 18000
+  calc_time = input_time.to_i - epoch_offset + gmt_offset #time diff between telemetry and tool
 
   #use following line instead of other calc_time for unix epoch (1970)
   #calc_time = input_time.to_i
