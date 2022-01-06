2fb817e66ee7b105a59ac2f428bd83f9537968d5
Jackie Smedley <jacqu2@umbc.edu>
2022-01-06 08:37:19 -0500

    whoops3

--- gen_ats_file_xsum.rb
+++ gen_ats_file_xsum.rb
@@ -36,7 +36,7 @@ def conv_epoch(input_time)
   offset_j2000 = 1893491975
 
   # modify this line if changing epoch
-  calc_time = input_time.to_i + epoch_offset - offset_j2000
+  calc_time = input_time.to_i + epoch_offset - offset_j2000 + 36857
 
   # 1980
   # calc_time = input_time.to_i + offset_1980 + epoch_offset
