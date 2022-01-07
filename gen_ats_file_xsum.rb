7f906f7cd659621b94f978b86e4db85bcc64a132
Jackie Smedley <jacqu2@umbc.edu>
2022-01-07 08:37:28 -0500

    took out time offset

--- gen_ats_file_xsum.rb
+++ gen_ats_file_xsum.rb
@@ -36,7 +36,10 @@ def conv_epoch(input_time)
   offset_j2000 = 1893491975 + 36857
 
   # modify this line if changing epoch
-  calc_time = input_time.to_i + epoch_offset - offset_j2000
+  # calc_time = input_time.to_i + epoch_offset - offset_j2000
+
+  # hard coded for j2000
+  calc_time = input_time.to_i
 
   # 1980
   # calc_time = input_time.to_i + offset_1980 + epoch_offset
